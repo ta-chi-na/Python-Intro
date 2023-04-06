3bc2cd3a65e1b126987678ee94a6ca6ff04c2e39
ta.chi.na <120833420+ta-chi-na@users.noreply.github.com>
2023-04-06 19:42:58 -0400

    Print Function
    You don't need to specify a new line in the print function!!!

---
+++ argv.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+if len(argv) == 2:
+    print(f"hello, {argv[1]}")
+else:
+    print("hello, world")
+
+# Python/ $ python argv.py
+# hello, world
+# Python/ $ python argv.py jess
+# hello, jess
+
+
+#Python/ $ python argv.py
+#hello, world
